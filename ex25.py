db38231a612db5f3f974d045ba9cfc54f9d07b54
chengcheng <cc20150921@gmail.com>
2017-08-12 02:59:31 +0800

    作业

---
+++ ex25.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+
+def break_words(stuff):
+    """This function will break up words for us."""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """Sorts the words."""
+    return sorted(words)
+
+def print_first_word(words):
+    """Prints the first word after popping it off."""
+    word = words.pop(0)
+    # s.pop() retrieves the item and removes it from s
+    # pop 参数中0代表第一个位置；负数就是位置倒过来数
+    # 参数的大小超出数组的个数范围将报错
+    print word
+
+def print_last_word(words):
+    """Prints the last word after popping it off."""
+    word = words.pop(-1)
+    print word
+
+def sort_sentence(sentence):
+    """Takes in a full sentence and return the sorted words."""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """Prints the first and last words of the sentence."""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
+# ^ D 关闭编译器
